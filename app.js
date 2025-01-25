6b93687353d247d76731d910e170f0e2b71ca07b
takashiono2 <takashiono710@gmail.com>
2025-01-25 13:28:18 +0900

    section5_31

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 const tasksRoute = require('./routes/tasks');
+const connectDB = require('./db/connect');
+
+require('dotenv').config();
+
 const POAT = 3000;
 
 app.use('/api/vi/tasks', tasksRoute);
 
-app.listen(POAT, console.log(`Server is running on port ${POAT}`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(POAT, console.log(`Server is running on port ${POAT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
