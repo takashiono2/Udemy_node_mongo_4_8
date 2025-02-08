8ef73b3fd190f2798da5ef54c234c64884fa91d6
takashiono2 <takashiono710@gmail.com>
2025-02-09 01:47:53 +0900

    section6_50

--- public/script.js
+++ public/script.js
@@ -1,9 +1,16 @@
 const tasksDOM = document.querySelector('.tasks');
+const formDOM = document.querySelector('.task-form');
+const taskInputDOM = document.querySelector('.task-input');
+const formAlertDOM = document.querySelector('.form-alert');
 
 const showTasks = async () => {
   try {
     const { data: tasks } = await axios.get('/api/v1/tasks');
-    const { name } = tasks;
+
+    if (tasks.length < 1) {
+      tasksDOM.innerHTML = `<h5 class="empty-list">タスクがありません</h5>`;
+      return;
+    }
     const allTasks = tasks.map((task) => {
       const { completed, _id, name } = task;
       return `
@@ -18,7 +25,7 @@ const showTasks = async () => {
           <a href="#" class="edit-link">
             <i class="fa fa-edit"></i>
           </a>
-          <button type="button" class="delete-btn">
+          <button type="button" class="delete-btn" data-id="${_id}">
             <i class="fa fa-trash"></i>
           </button>
         </div>
@@ -32,3 +39,38 @@ const showTasks = async () => {
 };
 
 showTasks();
+
+//タスクを追加する
+formDOM.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const name = taskInputDOM.value;
+  try {
+    await axios.post('/api/v1/tasks', { name: name });
+    showTasks();
+    taskInputDOM.value = '';
+    formAlertDOM.style.display = "block";
+    formAlertDOM.textContent = "タスクを追加しました";
+    formAlertDOM.classList.add("text-success");
+  } catch (err) {
+    console.log(err);
+    formAlertDOM.style.display = "block";
+    formAlertDOM.innerHTML = '無効です。もう一度お試しください';
+  }
+  setTimeout(() => {
+    formAlertDOM.style.display = "none";
+    formAlertDOM.classList.remove("text-success");
+  }, 3000);
+});
+//タスクを削除する
+tasksDOM.addEventListener('click', async (e) => {
+  const element = e.target;
+  if (element.parentElement.classList.contains('delete-btn')) {
+    try {
+      const id = element.parentElement.dataset.id;
+      await axios.delete(`/api/v1/tasks/${id}`);
+      showTasks();
+    } catch (err) {
+      console.log(err)
+    }
+  }
+});
