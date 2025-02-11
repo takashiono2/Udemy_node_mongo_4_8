1d3a63f83cee319220444c06e5ea1cf8ade142a0
takashiono2 <takashiono710@gmail.com>
2025-02-11 16:28:10 +0900

    section7_58

--- public/script.js
+++ public/script.js
@@ -13,7 +13,6 @@ const showTasks = async () => {
     }
     const allTasks = tasks.map((task) => {
       const { completed, _id, name } = task;
-      //completedがtrueの場合、"task-completed"クラスを追加
       return `
             <div class="single-task ${completed && "task-completed"}">
         <h5>
